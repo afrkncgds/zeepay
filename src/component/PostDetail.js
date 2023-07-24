2d9877a87a91a36afed93124257905500210e077
afrkncgds <76846024+afrkncgds@users.noreply.github.com>
2023-07-25 00:24:14 +0300

    updated

--- src/component/PostDetail.js
+++ src/component/PostDetail.js
@@ -1,14 +1,14 @@
 // components/PostDetail.js
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import {Link, useParams} from 'react-router-dom';
 
 const PostDetail = () => {
     const { postId } = useParams();
     const [post, setPost] = useState(null);
 
-    useEffect(async() => {
-       await axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+    useEffect(() => {
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`)
             .then(response => {
                 setPost(response.data);
             })
@@ -23,11 +23,17 @@ const PostDetail = () => {
 
     return (
         <div className="post-container">
-            <h2>{post.title}</h2>
-            <div className="post">
-                {post.body}
-            </div>
+
+                    <tr className="post-detail" key={post.id}>
+                        <td>{post.id}</td>
+                        <td><Link to={`/posts/${post.id}`}>{post.title}</Link></td>
+                        <td>{post.body}</td>
+                    </tr>
+
+
+
         </div>
+
     );
 };
 
